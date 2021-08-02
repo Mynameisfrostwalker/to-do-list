0ea87cf349cca826916718e9f5561f3fec79e2e6
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-08-02 14:06:04 +0100

    created inital page

--- src/scripts/initialpage.js
+++ src/scripts/initialpage.js
@@ -8,11 +8,9 @@ const head = (
         myFan.innerHTML = '<i class="fas fa-fan"></i>'
         const para = document.createElement('p');
         para.textContent = 'Your To-Do-List';
-        const create = () => {
-            header.appendChild(myFan);
-            header.appendChild(para);
-        }
-        return { create, header }
+        header.appendChild(myFan);
+        header.appendChild(para);
+        return { header }
     }
 )()
 
@@ -20,6 +18,57 @@ const aside = (
     function() {
         const sidebar = document.createElement('div');
         sidebar.setAttribute('id', 'sidebar');
+        const oneli = document.createElement('ul');
+        oneli.setAttribute("id", 'oneli');
+        const init = undefined;
+        const today = undefined;
+        const thisWeek = undefined;
+        const newProject = undefined;
+        const plus = undefined;
+        const createLi = (name, element, text) => {
+            name = document.createElement('li');
+            name.classList.add('nav');
+            if (text === '') {
+                name.setAttribute('id', 'new');
+                const ul = document.createElement('ul');
+                name.appendChild(ul);
+            } else { name.setAttribute('id', text); }
+            const i = document.createElement('i')
+            i.innerHTML = element
+            name.appendChild(i)
+            const words = document.createElement('p');
+            words.textContent = text;
+            name.appendChild(words);
+            oneli.appendChild(name);
+        }
+        createLi(init, '<i class="fas fa-tasks"></i>', 'Default');
+        createLi(today, '<i class="fas fa-calendar-day"></i>', 'Today');
+        createLi(thisWeek, '<i class="fas fa-calendar-week">', 'This Week');
+        createLi(newProject, '<i class="fas fa-project-diagram">', 'Projects');
+        createLi(plus, '<i class="fas fa-plus">', '')
+        sidebar.appendChild(oneli)
+        return { sidebar }
+    }
+)()
+
+const body = (
+    function() {
+        const bod = document.createElement('div');
+        bod.setAttribute('id', 'bod');
+        const bodHead = document.createElement('div');
+        bodHead.setAttribute('id', 'bodHead')
+        const bodHeadp = document.createElement('p');
+        bodHeadp.textContent = 'Default Project';
+        bodHead.appendChild(bodHeadp);
+        const tasks = document.createElement('div');
+        tasks.setAttribute('id', 'bodTasks');
+        const add = document.createElement('div');
+        add.setAttribute('id', 'bodAdd');
+        add.innerHTML = '<i class="fas fa-plus">';
+        bod.appendChild(bodHead);
+        bod.appendChild(tasks);
+        bod.appendChild(add);
+        return { bod }
     }
 )()
 
@@ -27,8 +76,9 @@ const display = (
     function() {
         const container = document.querySelector('div#container');
         const create = () => {
-            head.create();
-            container.appendChild(head.header)
+            container.appendChild(head.header);
+            container.appendChild(aside.sidebar);
+            container.appendChild(body.bod);
         }
         return { create }
     }
